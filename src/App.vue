ac097b18523dd12df6daf19050ceb0e0237559eb
kiraGit67 <kirsten@sais.de>
2023-03-30 13:17:07 +0200

    imports Header- and BookListComponent into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <header-component />
+  <book-list-component />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import BookListComponent from "@/components/BookListComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HeaderComponent,
+    BookListComponent,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
