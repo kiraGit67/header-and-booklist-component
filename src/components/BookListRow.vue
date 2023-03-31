f57b1a1cef45885ae24943e08cef721a7260b221
kiraGit67 <kirsten@sais.de>
2023-03-31 11:04:04 +0200

    adds new BookListRow component

---
+++ src/components/BookListRow.vue
@@ -0,0 +1,31 @@
+<template>
+  <tr class="table-item__table-row">
+    <td>{{ title }}</td>
+    <td>{{ author }}</td>
+    <td>{{ isbn }}</td>
+    <td>{{ numPages }}</td>
+    <td><BaseButton text="Add Bookmark" /></td>
+  </tr>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+  props: {
+    title: String,
+    author: String,
+    isbn: String,
+    numPages: String,
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
