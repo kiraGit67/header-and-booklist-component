5a7dc33bc327d350d5f1ea60bbae6f73bf804665
kiraGit67 <kirsten@sais.de>
2023-03-31 11:04:37 +0200

    adds BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <button :class="styling">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default: "primary",
+      validator(propValue) {
+        return ["primary", "secondary"].includes(propValue);
+      },
+    },
+  },
+  computed: {
+    styling() {
+      return ["btn", `btn--${this.variant}`];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  border: 3px solid transparent;
+  border-radius: 3px;
+  padding: 5px;
+  cursor: pointer;
+}
+
+.btn--primary {
+  border-color: var(--primary);
+  background-color: transparent;
+  color: var(--primary);
+}
+
+.btn--primary:hover {
+  border-color: var(--primary-dark);
+  background-color: var(--primary);
+  color: white;
+}
+
+.btn--secondary {
+  border-color: var(--secondary);
+  background-color: transparent;
+  color: var(--secondary);
+}
+
+.btn--secondary:hover {
+  background-color: var(--secondary);
+  color: #000;
+}
+</style>
